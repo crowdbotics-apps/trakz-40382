3f90b64d6284bc54d5eab7372684d2787194fccd
Crowdbotics <team@crowdbotics.com>
2023-05-01 22:03:14 +0000

    Initial commit.

---
+++ backend/trakz_40382/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trakz_40382 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trakz_40382.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trakz-40382",
+  url: "https://trakz-40382.botics.co",
+  api: "https://trakz-40382.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
